3b774a57fce2607a43a19204bac80baa330867b1
nidhinlalcv <69187545+nidhinlalcv@users.noreply.github.com>
2021-08-22 10:32:09 -0700

    Update sample.py
    one more condition; No

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 print("Hello world")
 response = input("Are you hungry?")
-if response == "yes":
+if response == "yes" and response != 'No':
     print("Eat PIZZA")
     print("Eat BIRIYANI")
     print("Eat Fries")
