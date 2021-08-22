9c07b6f906fd3b07e95128c1dc3d712334f74fb4
Nidhin Lal C V <nidhinlalcv@gmail.com>
2021-08-21 22:08:13 -0700

    water added

--- sample.py
+++ sample.py
@@ -1,7 +1,9 @@
 print("Hello world")
-response = input("Are you Hungry?")
+response = input("Are you hungry?")
 if response == "yes":
     print("Eat PIZZA")
     print("Eat BIRIYANI")
 else:
-    print("Do your work")
+    thirsty = input("Are you thirsty")
+    if thirsty == 'yes':
+        print("Drink water")
